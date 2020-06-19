3933be480e3b3f019f2a29d12135c13aa55d058f
Yana <kolodyazhnaya19@yandex.ru>
2020-06-19 17:49:12 +0300

    header

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-window.onscroll = function showheader() {
+window.onscroll = function showHeader() {
     var header = document.querySelector ('.header');
-    if(window.pageYOffset > 700) {
+    if(window.pageYOffset > window.innerHeight) {
         header.classList.add('header_fixed');
     } else {
         header.classList.remove('header_fixed');
     }
 };
+
+// window.onscroll = function showHeader() - данная функция выполняется при скроллинге страницы
+// onscroll - когда экран прокручивается
+// document.querySelector - находит первый элемент на странице с таким классом
+// window.pageYOffset - количество пикселей, на которое прокручен документ по вертикали (вниз или вверх)
+// header.classList.add('header_fixed') - добавляет элменту header класс header_fixed
+// remove - удаляет от элемента класс
+// window - объект, хранит всю информацию о нашем окне
+

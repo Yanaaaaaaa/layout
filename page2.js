5ac571a4a18316bd34b839bd373a688b542885eb
Yana <kolodyazhnaya19@yandex.ru>
2020-06-20 18:19:12 +0300

    layout

--- page2.js
+++ page2.js
@@ -15,4 +15,5 @@ document.getElementById("menu_burger").onclick = function() {
 
 function open() {
     document.getElementById("menu").classList.toggle("active");
+    document.getElementById("menu_burger").classList.toggle("active");
 };
